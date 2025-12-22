3e7d431dcc7f8fd4bdddff5849fbc60d2ff6312c
Chicão Vieira <fthiagogv@gmail.com>
2025-12-22 13:41:30 -0300

    adicionando a branch master a nerd-treesitter plugin

--- init.lua
+++ init.lua
@@ -941,6 +941,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    branch = 'master', 
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
